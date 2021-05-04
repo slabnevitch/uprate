5cd57f9762cc65c69d7a57c586ccfd71a82cf945
slabnevitch <handofschpilrain@mail.ru>
2021-05-04 17:10:29 +0500

    change calendar view to simple scroll over the month

---
+++ js/chunk-2d0cbb04.4e03e0ac.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cbb04"],{"4b77":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("MeetingsMain")},c=[],i=t("3a71"),s={name:"HistoryMainScreen",components:{MeetingsMain:i["a"]}},o=s,r=t("2877"),u=Object(r["a"])(o,a,c,!1,null,null,null);e["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d0cbb04.4e03e0ac.js.map
