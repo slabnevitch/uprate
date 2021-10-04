c43e93ac353d38153887ae9dc13f8497fce4d1c9
slabnevitch <handofschpilrain@mail.ru>
2021-10-04 22:20:08 +0500

    bourbone +font-face failed

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@
 #app{
 	height: 100%;
 }
-@import '@/assets/scss/misc/fonts.sass';
-@import '@/assets/scss/misc/_vars.scss';
-@import '@/assets/scss/misc/_mixins.scss';
-@import '@/assets/scss/misc/_adjustment.scss';
+// @import '@/assets/scss/misc/fonts.scss';
+// @import '@/assets/scss/misc/_vars.scss';
+// @import '@/assets/scss/misc/_mixins.scss';
+// @import '@/assets/scss/misc/_adjustment.scss';
 
 @import '@/assets/scss/main-page.scss';
 @import '@/assets/scss/meetings-page.scss';
